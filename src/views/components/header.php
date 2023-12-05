794a490b80dfdcd356ce462664ab80006924b620
async-devil <async.devil@gmail.com>
2023-12-06 00:40:13 +0100

    feat: update header

--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,1 +1,18 @@
-<header>Header</header>
+<?php
+require_once __DIR__ . "/../../common/Router/urlBuilder.php";
+
+use function App\urlBuilder;
+?>
+<header>
+  <div class="wrapper">
+    <div class="logo">
+      <b>ZWA</b> Recipes
+    </div>
+    <nav>
+      <ul>
+        <li><a href="<?= urlBuilder($GLOBALS["PREFIX"], "recipes/catalog/all") ?>">Recipes</a></li>
+        <li><a href="<?= urlBuilder($GLOBALS["PREFIX"], "admin") ?>">Admin</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
