2b39d4cec2d1a14828e26c52e56f846892abd45c
async-devil <async.devil@gmail.com>
2023-12-06 00:39:59 +0100

    feat: update recipe page

--- src/views/recipe.php
+++ src/views/recipe.php
@@ -15,26 +15,27 @@ require_once __DIR__ . '/components/head.php';
 </head>
 
 <body>
-  <div class="preview">
-    <div class="preview__text">
-      <div class="preview__text_wrapper">
-        <h1><?= $recipe->name ?></h1>
-        <p><?= $recipe->description ?></p>
+  <?php require_once __DIR__ . "/components/header.php"; ?>
+  <main>
+    <section class="preview">
+      <div class="preview__text">
+        <div class="preview__text_wrapper">
+          <h1><?= $recipe->name ?></h1>
+          <p><?= $recipe->description ?></p>
+        </div>
       </div>
-    </div>
-    <div class="preview__image"><img src="<?= $recipe->previewImage ?>" alt="Recipe preview image"></div>
-  </div>
-  <article>
-    <a href="<?= $recipe->source ?>">Original recipe</a>
-    <?php
-    foreach ($recipe->sections as $section) {
-      echo "<section>";
-      echo "<h2>{$section["name"]}</h2>";
-      echo $section["content"];
-      echo "</section>";
-    }
-    ?>
-  </article>
+      <div class="preview__image"><img src="<?= $recipe->previewImage ?>" alt="Recipe preview image"></div>
+    </section>
+    <section class="recipe">
+      <a href="<?= $recipe->source ?>">Original recipe</a>
+      <?php foreach ($recipe->sections as $section) : ?>
+        <article>
+          <h2><?= $section["name"] ?></h2>
+          <p><?= $section["content"] ?></p>
+        </article>
+      <?php endforeach ?>
+    </section>
+  </main>
 </body>
 
 </html>
