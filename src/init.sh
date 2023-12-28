5e343dcdf25a904462e1965879b60ce1fedb3363
async-devil <async.devil@gmail.com>
2023-12-28 23:32:27 +0100

    feat: add init script

---
+++ src/init.sh
@@ -0,0 +1,8 @@
+touch common/Authentication/credentials.env
+chmod 777 common/Authentication/credentials.env
+
+echo "[]" > common/Database/database.json
+chmod 777 common/Database/database.json
+
+mkdir -p public/assets/uploads
+chmod 777 public/assets/uploads
